2cf9cc5410ce943982952dcbdfb539aca02e6617
001ZTA744 <001ZTA744@LAPTOP-M75M3NST>
2021-06-10 15:18:18 +0530

    Added interface and abstract classes

--- 10_06_21_Static_import/src/comm/example/Airplane.java
+++ 10_06_21_Static_import/src/comm/example/Airplane.java
@@ -1,48 +1,40 @@
 package comm.example;
 
-public class RiverBarge extends Vehicle implements Sailer{
-	private double startPoint;
+public abstract class Airplane extends Vehicle {
+   private double startPoint;
 	private double endPoint;
 	private double distanceCovered;
 	private double fuelEfficiency;
 	@Override
 	public double calcFuelEfficiency(double fuelConsumed) {
-		// TODO Auto-generated method stub
-		
-	fuelEfficiency= distanceCovered/fuelConsumed;
-	return fuelEfficiency;
+		fuelEfficiency = distanceCovered / fuelConsumed;
+		return fuelEfficiency;
 	}
-
 	@Override
 	public double calcTripDistance(double startPoint, double endPoint) {
-		// TODO Auto-generated method stub
-		this.startPoint=startPoint;
-		this.endPoint=endPoint;
-		distanceCovered=this.endPoint-this.startPoint;
+		this.startPoint = startPoint;
+		this.endPoint = endPoint;
+		distanceCovered = this.endPoint - this.startPoint;
 		return distanceCovered;
 	}
-
 	@Override
 	public String getDetails() {
-		// TODO Auto-generated method stub
-		return super.getDetails() +" RiverBarge-->";
+		return super.getDetails() + " RiverBarge-->";
 	}
-
 	@Override
 	public String getMessage() {
 		// TODO Auto-generated method stub
 		return super.getMessage();
 	}
-
 	@Override
 	public void setMessage(String message) {
 		// TODO Auto-generated method stub
 		super.setMessage(message);
 	}
 	
-	public void dock() {
-		System.out.println("Give RiverBarge Permission to Dock.");
-		
-	}
+	public abstract void takeoff();
+	public abstract void land();
+	public abstract void fly();
+	
 	
 }
--- 10_06_21_Static_import/src/comm/example/Tester.java
+++ 10_06_21_Static_import/src/comm/example/Tester.java
@@ -3,7 +3,7 @@ package comm.example;
 public class Tester {
 
 	public static void main(String[] args) {
-		
+		/*
 		Vehicle myVehicle = new Truck();
 		System.out.println(myVehicle.calcTripDistance(100.9, 900.8));
 		System.out.println(myVehicle.calcFuelEfficiency(45));
@@ -15,6 +15,6 @@ public class Tester {
 		myVehicle.setMessage("I Am On Riverbarge");
 		System.out.println(myVehicle.getMessage());
 
-		
+		*/
 	}
 }
