6486566119b771682410827a10f8ba2ad3cfa489
pooja yadav <kuhoo97@gmail.com>
2021-06-21 18:19:25 +0530

    maven and hibernate

---
+++ 18th june/account-service/src/main/java/org/example/demo/dao/AccountDAO.java
@@ -0,0 +1,19 @@
+package org.example.demo.dao;
+
+import java.util.List;
+import java.sql.SQLException;
+import org.example.demo.dto.AccountDTO;
+import org.example.demo.model.Account;
+
+public interface AccountDAO {
+
+	
+	public Account createAccount(Account account)throws SQLException;
+	public List<AccountDTO> displayAllAccount()throws SQLException;
+	public void deposite(double amount) throws SQLException;
+	public double withdrawl(double amount) throws SQLException;
+	
+	
+	
+	
+}
