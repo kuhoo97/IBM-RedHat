f1954de350c7e18916dc4addb8bea166a23a7c94
kuhoo97 <40567146+kuhoo97@users.noreply.github.com>
2021-06-30 11:15:35 +0530

    Add files via upload

---
+++ 30th june/javascript-examples/app.js
@@ -0,0 +1,35 @@
+// we are using let instead of var
+
+let _firstName='John';
+let age=78;
+const _MAX=100;
+_firstName=true;
+console.log(_firstName);
+console.log(age);
+
+console.log(_MAX);
+let num = 10;
+
+console.log(num);
+
+// Increment operator
+num++; // num = num + 1
+
+console.log(num);
+
+var num2 = 10;
+
+console.log(num2);
+
+// Decrement operator
+num2--; // num2 = num2 - 1
+
+console.log(num2);
+
+let num3 = 40;
+
+console.log(num3);
+
+console.log(--num3);
+
+console.log(num3);
