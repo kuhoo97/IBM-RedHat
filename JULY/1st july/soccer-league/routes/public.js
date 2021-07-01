0c28fc6128ffc8474e7b028337d657860a885f8a
pooja yadav <kuhoo97@gmail.com>
2021-07-01 17:52:10 +0530

    modified soccer and vidly using router

---
+++ JULY/1st july/soccer-league/routes/public.js
@@ -0,0 +1,20 @@
+const path=require('path');
+const express=require('express');
+const app=express();
+const router=express.Router();
+router.get('/display-league',(req,res,next)=>{
+
+    res.sendFile(path.join(__dirname,'../','views','display-league.html'));
+
+});
+router.get('/register-league',(req,res,next)=>{
+    res.sendFile(path.join(__dirname,'../','views','register-league.html'));
+});
+
+router.post('/register-league',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+
+module.exports=router;
