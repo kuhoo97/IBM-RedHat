498024ded0904bcf82cde173841a5a8ccf2ce5be
pooja yadav <kuhoo97@gmail.com>
2021-07-09 18:14:01 +0530

    creating two instances of the same service using server:0

--- JULY/9th july/Microservices 2/hello-world-service/src/main/java/com/example/demo/controller/HelloWorldController.java
+++ JULY/9th july/Microservices 2/hello-world-service/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,14 +1,23 @@
 package com.example.demo.controller;
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	private Environment environment;
+	
+
+	public HelloWorldController(Environment environment) {
+	
+		this.environment = environment;
+	}
 
 	@GetMapping("/")
 	public String sayHello()
 	{
-		return "hello-world";
+		return "hello-world application is running on port number: "+environment.getProperty("local.server.port");
 	}
 }
