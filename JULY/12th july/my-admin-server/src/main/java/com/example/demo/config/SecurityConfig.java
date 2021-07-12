65ac9ba36d380be1d6af3edb6e6ce6c984a88ca6
pooja yadav <kuhoo97@gmail.com>
2021-07-12 17:44:38 +0530

    Microservices and spring security

---
+++ JULY/12th july/my-admin-server/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+/*
+ * package com.example.demo.config;
+ * 
+ * import
+ * org.springframework.security.config.annotation.web.builders.HttpSecurity;
+ * import org.springframework.security.config.annotation.web.configuration.
+ * WebSecurityConfigurerAdapter;
+ * 
+ * public class SecurityConfig extends WebSecurityConfigurerAdapter {
+ * 
+ * 
+ * @Override protected void configure(HttpSecurity http) throws Exception { //
+ * TODO Auto-generated method stub http.csrf().disable(); http.cors().disable();
+ * http.csrf().ignoringAntMatchers("/**"); super.configure(http); }
+ * 
+ * 
+ * }
+ */
